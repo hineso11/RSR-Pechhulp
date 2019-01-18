c625191883deeab917f40bc451099500245c851e
hineso11 <oliver.hines@btinternet.com>
2019-01-18 13:08:30 +0000

    Setup basic UI and added resources

---
+++ RSR Pechhulp/RSR Pechhulp/MenuViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MenuViewController.swift
+//  RSR Pechhulp
+//
+//  Created by Oliver Hines on 17/01/2019.
+//  Copyright © 2019 Oliver Hines Apps. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    // MARK: Properties
+    @IBOutlet weak var informationButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //let informationImage = UIImage(named: "InformationImage")?.withRenderingMode(.alwaysOriginal)
+        //informationButton.image = informationImage
+    }
+
+}Ω
